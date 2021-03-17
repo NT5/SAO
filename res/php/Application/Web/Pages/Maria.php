ff9a56b3e2865253c3774103c9d9f124f0a33a5e
MariaJ <est.mcascod@uml.edu.ni>
2021-03-17 14:59:12 -0600

    Se creo un controlador para las asignaturas

--- res/php/Application/Web/Pages/Maria.php
+++ res/php/Application/Web/Pages/Maria.php
@@ -6,34 +6,40 @@ use SAO\Modules\Extended;
 use SAO\Modules\WebPage\Page;
 use SAO\Modules\App\Carreras;
 use SAO\Modules\App\Asignaturas;
+use SAO\Modules\App\Municipios;
 
 class Maria extends Page {
 
     private $Carreras;
     private $Asignaturas;
-    
+    private $Municipios;
+
     /**
      * Es el constructor de la pagina
      * @param Extended $Extended
      */
     public function __construct(Extended $Extended = NULL) {
         parent::__construct($Extended, "Maria", "pages/Maria.twig");
-        
+
         $this->Carreras = new Carreras($this->Extended());
         $this->Asignaturas = new Asignaturas($this->Extended());
+        $this->Municipios = new Municipios($this->Extended());
         $this->initVars();
     }
-    
+
     public function initVars() {
         parent::initVars();
-        
+
         $listaCarreras = $this->Carreras->getListaCarreras();
         $this->setVar('listaCarreras', $listaCarreras);
-        
-        $listaAsignaturas= $this->Asignaturas->getListaAsignaturas();
+
+        $listaAsignaturas = $this->Asignaturas->getListaAsignaturas();
         $this->setVar('listaAsignaturas', $listaAsignaturas);
+
+        $listaMunicipios = $this->Municipios->getListaMunicipios();
+        $this->setVar('listaMunicipios', $listaMunicipios);
     }
-    
+
     public function CheckPost() {
         parent::CheckPost();
         $post = $this->getPost();
---
+++ res/php/Modules/App/Municipios.php
@@ -0,0 +1,23 @@
+<?php
+namespace SAO\Modules\App;
+
+use SAO\Modules\Extended\ExtendedExtended;
+
+
+
+
+
+class Municipios extends ExtendedExtended {
+    public function getListaMunicipios() {
+        $db = $this->Extended()->Database();
+        $stmt = $db->prepare("SELECT Id_Ped, Nombre FROM peds");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $list = [];
+        while ($row = $result->fetch_assoc()) {
+            $list[] = $row;
+        }
+        return $list;
+    }
+    //put your code here
+}
