3a5f8430c6593225b7ffb4a3044235ee62025cfc
MariaJ <est.mcascod@uml.edu.ni>
2021-04-15 14:06:15 -0600

    Agregada la estructura de la tabla para las entradas del silabo

--- res/sql/tables/Silabo.sql
+++ res/sql/tables/Silabo.sql
@@ -15,3 +15,35 @@ CREATE TABLE `Silabo_Entradas` (
  CONSTRAINT FOREIGN KEY (`Id_Ped`) REFERENCES `Peds` (`Id_Ped`) ON UPDATE CASCADE ON DELETE CASCADE,
  CONSTRAINT FOREIGN KEY (`Createby`) REFERENCES `User_Entries` (`User_Id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+
+-- Tabla de entradas alternativa
+CREATE TABLE `silabo_entradas` (
+	`Id_Silabo` INT(11) NOT NULL AUTO_INCREMENT,
+	`Id_Ped` INT(11) NOT NULL,
+	`Id_Carrera` INT(11) NOT NULL,
+	`Id_Asignatura` INT(11) NOT NULL,
+	`CodigoGrupo` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+	`CodAsignatura` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+	`AnioCarrera` INT(11) NULL DEFAULT NULL,
+	`HorasClase` INT(11) NOT NULL,
+	`HorasPrecenciales` INT(11) NOT NULL,
+	`HorasIndependientes` INT(11) NOT NULL,
+	`Modalidad` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+	`Trimestre` INT(11) NOT NULL,
+	`TipoEvaluacion` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+	`AnioLectivo` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
+	`Facilitador` VARCHAR(250) NOT NULL COLLATE 'latin1_swedish_ci',
+	`Editedat` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	`Createat` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+	PRIMARY KEY (`Id_Silabo`) USING BTREE,
+	INDEX `Id_Asignatura` (`Id_Asignatura`) USING BTREE,
+	INDEX `Id_Carrera` (`Id_Carrera`) USING BTREE,
+	INDEX `Id_Ped` (`Id_Ped`) USING BTREE,
+	CONSTRAINT `silabo_entradas_ibfk_1` FOREIGN KEY (`Id_Asignatura`) REFERENCES `uml_sao`.`asignaturas` (`Id_Asignatura`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `silabo_entradas_ibfk_2` FOREIGN KEY (`Id_Carrera`) REFERENCES `uml_sao`.`carreras` (`Id_Carrera`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `silabo_entradas_ibfk_4` FOREIGN KEY (`Id_Ped`) REFERENCES `uml_sao`.`peds` (`Id_Ped`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
